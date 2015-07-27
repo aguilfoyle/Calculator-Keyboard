c523e2cdcdb0d9315d458bab255805c7e05f3a0c
Alan Guilfoyle <SirWalter@Alans-MacBook-Pro.local>
2015-07-27 18:47:19 -0400

    Initial Commit

---
+++ Calc Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc Keyboard
+//
+//  Created by Alan Guilfoyle on 7/27/15.
+//  Copyright (c) 2015 Think Thrice Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
