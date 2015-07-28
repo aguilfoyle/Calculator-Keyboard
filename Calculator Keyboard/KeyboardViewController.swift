f61b7da04a639ff7c6496a54d05bce0983e20984
Alan Guilfoyle <SirWalter@Alans-MacBook-Pro.local>
2015-07-27 20:12:02 -0400

    First Commit;

---
+++ Calculator Keyboard/KeyboardViewController.swift
@@ -0,0 +1,42 @@
+//
+//  KeyboardViewController.swift
+//  Calculator Keyboard
+//
+//  Created by Alan Guilfoyle on 7/27/15.
+//  Copyright (c) 2015 Think Thrice Tech. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController 
+{
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() 
+	{
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+
+		let nib = UINib(nibName: "KeyboardView", bundle: nil)
+		let objects = nib.instantiateWithOwner(self, options: nil)
+		
+		view = objects[0] as! UIView;
+		
+	}
+
+	@IBAction func next(sender: AnyObject) 
+	{
+		advanceToNextInputMode()
+	}
+
+    override func textWillChange(textInput: UITextInput) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+}
